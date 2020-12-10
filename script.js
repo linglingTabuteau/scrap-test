4502df77f46ca7bd242e57f2ffd1eb71d1263318
Lingling Tabuteau <linging.tabuteau@gmail.com>
2020-12-10 16:08:16 +0100

    change to method async await to scrap data

--- script.js
+++ script.js
@@ -1,57 +1,42 @@
-const request = require("request");
+// const request = require("request");
+const fetch = require("node-fetch");
 const cheerio = require("cheerio");
 const fs = require("fs");
 const iconv = require("iconv");
 
 let allProducts = [];
 
-for (let nbPage = 1; nbPage <= 24; nbPage++) {
-  request.get(
-    {
-      url: `https://www.ultrajeux.com/cat.php?cat=3&jeu=0&page=${nbPage}`,
-      encoding: null,
-    },
-    (error, response, html) => {
-      if (!error && response.statusCode == 200) {
-        // scrape encoding
-        const ic = new iconv.Iconv("iso-8859-1", "utf-8");
-        const buf = ic.convert(html);
-        const utf8String = buf.toString("utf-8");
+const scrapper = async () => {
+  for (let nbPage = 1; nbPage <= 24; nbPage++) {
+    console.log(nbPage);
+    let response = await fetch(
+      `https://www.ultrajeux.com/cat.php?cat=3&jeu=0&page=${nbPage}`
+    );
+    // let data = await response.json() why not json???
+    let data = await response.text();
 
-        const $ = cheerio.load(utf8String);
+    const $ = cheerio.load(data);
 
-        let oneObject = {};
-        $("#container_droite .block_produit").each((index, el) => {
-          const link = $(el).find("a").attr("href");
-          const title = $(el).find(".titre").text();
-          const prix = $(el).find(".prix").find(".prix").text();
-          const reduction = $(el).find(".titre_pourcent").text().split(" ")[1];
-          const image = $(el)
-            .find(".image")
-            .find("a")
-            .children("img")
-            .attr("src");
+    $("#container_droite .block_produit").each((index, el) => {
+      const oneProduct = {
+        link: $(el).find("a").attr("href"),
+        title: $(el).find(".titre").text(),
+        prix: $(el).find(".prix").find(".prix").text(),
+        // reduction: $(el).find(".titre_pourcent").text().split(" ")[1],
+        image: $(el).find(".image").find("a").children("img").attr("src"),
+      };
 
-          oneObject = reduction
-            ? {
-                url: `https://www.ultrajeux.com/${link}`,
-                name: title,
-                price: prix,
-                urlImage: image,
-                promotion: reduction,
-              }
-            : {
-                url: `https://www.ultrajeux.com/${link}`,
-                name: title,
-                price: prix,
-                urlImage: image,
-              };
-          allProducts.push(oneObject);
-          // console.log(allProducts.length);
-          let dataJson = JSON.stringify(allProducts);
-          fs.writeFileSync("myjsonfile.json", dataJson);
-        });
-      }
-    }
-  );
-}
+      let reduction = $(el).find(".titre_pourcent").text().split(" ")[1];
+      let result = reduction
+        ? (oneProduct["promotion"] = reduction)
+        : oneProduct;
+
+      allProducts.push(oneProduct);
+      console.log(oneProduct);
+    });
+  }
+  let dataJson = JSON.stringify(allProducts);
+  fs.writeFileSync("myFile.json", dataJson);
+};
+
+scrapper();
